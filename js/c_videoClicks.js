1d7f1989eb73d0466fa744525c686612d6ffc2b0
Vishal Waman <VishalW@winjit.local>
2018-01-18 19:04:19 +0530

    Video Fixes

--- js/c_videoClicks.js
+++ js/c_videoClicks.js
@@ -1,7 +1,7 @@
 
 function video1Click() {
     $('#videoSrc').fadeOut(0);
-    document.getElementById("videoSrc").src = "http://winjitstaging.cloudapp.net/botimages/IAGON_LowRes_V3.mp4";
+    document.getElementById("videoSrc").src = "https://www.youtube.com/embed/FdmCfSBkUyI";
     $('#videoSrc').fadeIn(1500);
     document.getElementById("vid1").src = "images/_vid_1_sel.png";
     document.getElementById("vid2").src = "images/vid_2.png";
@@ -12,7 +12,7 @@ function video1Click() {
 
 function video2Click() {
     $('#videoSrc').fadeOut(0);
-    document.getElementById("videoSrc").src = "http://winjitstaging.cloudapp.net/botimages/IAGON_LowRes_V3.mp4";
+    document.getElementById("videoSrc").src = "https://www.youtube.com/embed/FdmCfSBkUyI";
     $('#videoSrc').fadeIn(1500);
     // $('#videoSrc').fadeIn(1500).delay(3500).fadeOut(1500);
    
@@ -27,7 +27,7 @@ function video2Click() {
 function video3Click() {
 
     $('#videoSrc').fadeOut(0);
-    document.getElementById("videoSrc").src = "http://winjitstaging.cloudapp.net/botimages/IAGON_LowRes_V3.mp4";
+    document.getElementById("videoSrc").src = "https://www.youtube.com/embed/FdmCfSBkUyI";
     $('#videoSrc').fadeIn(1500);
     document.getElementById("vid1").src = "images/vid_1.png";
     document.getElementById("vid2").src = "images/vid_2.png";
@@ -37,7 +37,7 @@ function video3Click() {
 
 function video4Click() {
     $('#videoSrc').fadeOut(0);
-    document.getElementById("videoSrc").src = "http://winjitstaging.cloudapp.net/botimages/IAGON_LowRes_V3.mp4";
+    document.getElementById("videoSrc").src = "https://www.youtube.com/embed/FdmCfSBkUyI";
     $('#videoSrc').fadeIn(1500);
     document.getElementById("vid1").src = "images/vid_1.png";
     document.getElementById("vid2").src = "images/vid_2.png";
